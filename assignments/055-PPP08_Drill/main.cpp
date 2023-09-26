642b0be6b374400d075e97d7e1190d141562a28a
jbslanka <john.slankas@ncsu.edu>
2023-09-26 00:16:26 -0400

    54/55

---
+++ assignments/055-PPP08_Drill/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    X::var = 7;
+    X::print(); // print X’s var
+    using namespace Y;
+    var = 9;
+    print(); // print Y’s var
+    {
+        using Z::print;
+        using Z::var;
+        var = 11;
+        print(); // print Z’s var
+    }
+    print();    // print Y’s var
+    X::print(); // print X’s var
+}
